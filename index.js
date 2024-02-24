031c0c26d2c41ca6b1584d9b362aaca8cd662d0b
Kuba <339630@uwr.edu.pl>
2024-02-24 09:38:29 +0100

    Code cleanup + also pack .env.template

--- index.js
+++ index.js
@@ -11,22 +11,15 @@ app.set('views', './views');
 
 app.use(urlencoded({ extended: true }));
 
-// const config = {
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-//     idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-//     queueLimit: 0,
-//     enableKeepAlive: true,
-//     keepAliveInitialDelay: 0,
-// };
-
 const config = {
-    connectionLimit: 5,
-    connectTimeout: 10000, // 10 seconds
-    waitForConnections: true, // Default: true
-    queueLimit: 0, // Default: 0
-    // port: 3306
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    // idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    // queueLimit: 0,
+    // enableKeepAlive: true,
+    // keepAliveInitialDelay: 0,
+    // port: 3306,
 };
 
 console.log(process.env.DB_USER);
--- pack.sh
+++ pack.sh
@@ -1,1 +1,1 @@
-tar cvf baza.tar.gz views/* .dockerignore .env db.sql Dockerfile package-lock.json package.json index.js
+tar cvf baza.tar.gz views/* .dockerignore .env .env.template db.sql Dockerfile package-lock.json package.json index.js
